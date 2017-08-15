d55df5dab9ff5db52c17791b01d74535eca17baf
Administrator <Administrator@windows10.microdone.cn>
2017-08-15 10:17:21 +0800

    123

---
+++ src/com/tuzhi/app/entity/AppUserOrderAddress.java
@@ -0,0 +1,33 @@
+package com.tuzhi.app.entity;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class AppUserOrderAddress {
+	 
+	private int id;
+	private String address_id;
+	private String user_id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress_id() {
+		return address_id;
+	}
+	public void setAddress_id(String address_id) {
+		this.address_id = address_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+}
